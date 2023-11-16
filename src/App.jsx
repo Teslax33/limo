17b8fcf58d4e3cfad5a3495756b54010b3b3f748
Sadim Mali <sadimmalakar77@gmail.com>
2023-11-16 21:03:32 +0545

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import NavbarNav from './components/NavbarNav'
+import Hero from './components/Hero'
 const App = () => {
   return (
-    <div>App</div>
+    <>
+          <NavbarNav />
+          <Hero />
+    </>
   )
 }
 
