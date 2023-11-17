ae72cabaa8deb5d196f7c22f3f11543c0fc69771
Sadim Mali <sadimmalakar77@gmail.com>
2023-11-17 16:38:27 +0545

    Add fixed position to NavbarNav

--- src/components/NavbarNav.jsx
+++ src/components/NavbarNav.jsx
@@ -5,16 +5,16 @@ import Navbar from 'react-bootstrap/Navbar';
 
 const NavbarNav = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" fixed='top' className="bg-body-tertiary">
     <Container>
-      <Navbar.Brand href="#home">DIMOS</Navbar.Brand>
+      <Navbar.Brand href="/">DIMOS</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mx-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="/">Vehicles</Nav.Link>
-          <Nav.Link href="/">Blog</Nav.Link>
-          <Nav.Link href="/">Contacts</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/vehicles">Vehicles</Nav.Link>
+          <Nav.Link href="/blog">Blog</Nav.Link>
+          <Nav.Link href="/contacts">Contacts</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
