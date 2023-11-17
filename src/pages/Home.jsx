db01cc0917fed8ff5725dd2b686688044c2288ec
Sadim Mali <sadimmalakar77@gmail.com>
2023-11-17 16:40:09 +0545

    feat : Add components to Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import NavbarNav from '../components/NavbarNav'
+
+const Home = () => {
+  return (
+    <>
+    <NavbarNav />
+        <Hero />
+    </>
+  )
+}
+
+export default Home
