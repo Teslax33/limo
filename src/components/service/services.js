d75336de737f97e47a9cdb6b80cfb8685dfe9930
Sadim Mali <sadimmalakar77@gmail.com>
2023-11-20 21:39:52 +0545

    Add service details to services array with corresponding images

---
+++ src/components/service/services.js
@@ -0,0 +1,32 @@
+
+import airportTransferImg from "../../assets/Images/services/airport.jpg"
+import businessImg from "../../assets/Images/services/business.jpeg"
+import interCityImg from "../../assets/Images/services/interCity.jpg"
+import weddingImg from "../../assets/Images/services/wedding.jpg"
+
+export const services = [
+    {
+        id: 1,
+        title : "Airport transfers",
+        description : "With addtional wait time and fight tracking in case of delays, our service is optimized to make every airport transfer a breeze.",
+        img : airportTransferImg
+    },
+    {
+        id: 2,
+        title : "Intercity trips",
+        description : "Your stressfree solution for traveling between cites with chauffeurs all over the world",
+        img : interCityImg
+    },
+    {
+        id: 3,
+        title : "Wedding events",
+        description : "Our friendly, and attentive seriv ce combined with thorough attention to detail ensure yo ucan truly relax and enjoy your special day.",
+        img : weddingImg
+    },
+    {
+        id: 4,
+        title : "Business Meeting",
+        description : "Focus on your meeting with your patners, forget about the road and the car, which will distract your thoughts",
+        img : businessImg
+    }
+];
