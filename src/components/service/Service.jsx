3dc9ea1d44d59835d4667059b4cbdce2d7ddb9a1
Sadim Mali <sadimmalakar77@gmail.com>
2023-11-20 21:42:00 +0545

    Add services as ServiceCard in Service.jsx

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -3,32 +3,32 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import ServiceCard from "./ServiceCard";
 import "./Service.css";
+import { services } from "./services";
 
 const Service = () => {
   return (
     <section className="service pt-5">
       <Container>
-        <div className="service-heading d-flex align-items-center justify-content-center gap-4">
+        <div className="service-heading d-flex align-items-center justify-content-center gap-4 mb-5">
           <h1>Services</h1>
-          <p>
+          <p className="mb-0">
             {" "}
             We invite you to try our services, and we personally guarantee that
             you will be completely satisfied.
           </p>
         </div>
         <Row className="gx-2 gy-4">
-          <Col lg={6}>
-            <ServiceCard />
-          </Col>
-          <Col lg={6}>
-            <ServiceCard />
-          </Col>
-          <Col lg={6}>
-            <ServiceCard />
-          </Col>
-          <Col lg={6}>
-            <ServiceCard />
-          </Col>
+          {services.map((service) => {
+            return (
+              <Col key={service.id} lg={6}>
+                <ServiceCard
+                  title={service.title}
+                  description={service.description}
+                  img = {service.img}
+                />
+              </Col>
+            );
+          })}
         </Row>
       </Container>
     </section>
--- src/components/service/ServiceCard.jsx
+++ src/components/service/ServiceCard.jsx
@@ -1,15 +1,15 @@
 import Button from 'react-bootstrap/esm/Button'
 import "./Service.css";
 
-const ServiceCard = () => {
+const ServiceCard = ({title, description, img}) => {
   return (
-    <div className='service-card d-flex gap-4 w-100 rounded px-5 py-2'>
-        <div className="service-img">
-            <img src='' alt='service card images'/>
+    <div className='service-card d-flex gap-4 w-100 rounded px-3 py-3 align-items-center mb-5'>
+        <div className="service-img  mw-100">
+            <img src={img} alt='service card images' className='rounded' style={{maxWidth: "260px"}}/>
         </div>
         <div className="service-content">
-            <h2 className='sub-heading'>Airport transfers</h2>
-            <p>With addtional wait time and fight tracking in case of delays, our service is optimized to make every airport transfer a breeze.</p>
+            <h3 className='sub-heading fw-bold'>{title}</h3>
+            <p>{description}</p>
             <Button variant='dark'>Read More</Button>
         </div>
     </div>
