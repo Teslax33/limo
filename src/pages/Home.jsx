77903f4a6fd6ed90510b8cc06df3e226cced2408
Sadim Mali <sadimmalakar77@gmail.com>
2023-11-21 21:22:35 +0545

    Add PickCar component in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import NavbarNav from '../components/NavbarNav'
 import Service from '../components/service/Service'
+import PickCar from '../components/carPick/PickCar'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
     <NavbarNav />
         <Hero />
         <Service />
+        <PickCar />
     </>
   )
 }
