8583153eb283e85235183d5bb0ca90b1e17e0db5
Sadim Mali <sadimmalakar77@gmail.com>
2023-12-04 19:43:07 +0545

    feat : Add car images for selection

---
+++ src/components/image.js
@@ -0,0 +1,10 @@
+import audi from "../assets/Images/car-pick/audia.jpg";
+import benz from "../assets/Images/car-pick/benz.jpg";
+import bmw from "../assets/Images/car-pick/bmw320.jpg";
+import golf6 from "../assets/Images/car-pick/golf6.jpg";
+import passatcc from "../assets/Images/car-pick/passatcc.jpg";
+import toyotacamry from "../assets/Images/car-pick/toyotacamry.jpg";
+
+const images = [audi, benz, bmw, golf6, passatcc, toyotacamry];
+
+export default images;
