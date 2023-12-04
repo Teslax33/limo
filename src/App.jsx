822f56ac3023c1122ff970841af1f13c5fa45dd7
Sadim Mali <sadimmalakar77@gmail.com>
2023-12-04 19:44:02 +0545

    feat :

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { useState, createContext } from "react";
 import Home from "./pages/Home";
 
+export const CarContext = createContext("");
+
 export default function App() {
+  const [show, setShow] = useState(false);
+
   return (
-    <>
+    <CarContext.Provider value={{ show, setShow }}>
       <Home />
-    </>
+    </CarContext.Provider>
   );
 }
