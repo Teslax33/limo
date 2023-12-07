f56953f09a729ec098b4e1e1f89c7f9d8dc881c8
Sadim Mali <sadimmalakar77@gmail.com>
2023-12-07 20:21:48 +0545

    refactor: Add link router that renders the matching route

--- src/components/NavbarNav.jsx
+++ src/components/NavbarNav.jsx
@@ -1,19 +1,20 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 export default function NavbarNav() {
   return (
     <Navbar expand="lg" fixed="top" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">DIMOS</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">DIMOS</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/vehicles">Vehicles</Nav.Link>
-            <Nav.Link href="/blog">Blog</Nav.Link>
-            <Nav.Link href="/contacts">Contacts</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/vehicles">Vehicles</Nav.Link>
+            <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+            <Nav.Link as={Link} to="/contacts">Contacts</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
