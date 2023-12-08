8231bc1a06892c11f9bd299aa57dd5c34116dd5b
Sadim Mali <sadimmalakar77@gmail.com>
2023-12-08 19:44:49 +0545

    Add images as variables

--- src/components/carPick/CarData.js
+++ src/components/carPick/CarData.js
@@ -5,12 +5,20 @@ import Bmw320 from "../../assets/Images/car-pick/bmw320.jpg";
 import Benz from "../../assets/Images/car-pick/benz.jpg";
 import Passat from "../../assets/Images/car-pick/passatcc.jpg";
 
+import audi from "../../assets/Images/vehicle/audi-box.png";
+import benz from "../../assets/Images/vehicle/benz-box.png";
+import bmw from "../../assets/Images/vehicle/bmw-box.png";
+import golf6 from "../../assets/Images/vehicle/golf6-box.png";
+import passat from "../../assets/Images/vehicle/passat-box.png";
+import toyota from "../../assets/Images/vehicle/toyota-box.png";
+
 export const CAR_DATA = [
     [
       {
         name: "VW Golf 6",
         price: "37",
         img: Golf6,
+        img2: golf6,
         model: "Golf 6",
         mark: "Volkswagen",
         year: "2008",
@@ -25,6 +33,7 @@ export const CAR_DATA = [
         name: "Audi A1 S-Line",
         price: "45",
         img: AudiA1,
+        img2: audi, 
         model: "Audi",
         mark: "A1",
         year: "2012",
@@ -39,6 +48,7 @@ export const CAR_DATA = [
         name: "Toyota Camry",
         price: "30",
         img: Toyota,
+        img2 : toyota,
         model: "Camry",
         mark: "Toyota",
         year: "2006",
@@ -50,10 +60,11 @@ export const CAR_DATA = [
     ],
     [
       {
-        name: "BMW 320 ModernLine",
+        name: "BMW 320 ",
         price: "35",
         img: Bmw320,
-        model: "320",
+        img2: bmw,
+        model: "ModernLine",
         mark: "BMW",
         year: "2012",
         doors: "4/5",
@@ -67,6 +78,7 @@ export const CAR_DATA = [
         name: "Mercedes-Benz GLK",
         price: "50",
         img: Benz,
+        img2: benz,
         model: "Benz GLK",
         mark: "Mercedes",
         year: "2006",
@@ -81,6 +93,7 @@ export const CAR_DATA = [
         name: "VW Passat CC",
         price: "25",
         img: Passat,
+        img2: passat,
         model: "Passat CC",
         mark: "Volkswagen",
         year: "2008",
