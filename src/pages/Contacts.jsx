2155b054eb655e30001e98018a9b347785988ca1
Sadim Mali <sadimmalakar77@gmail.com>
2023-12-12 15:34:06 +0545

    feat : Add iframe for map

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,13 +1,25 @@
-import HeroBreadcrumb from "../components/HeroBreadcrumb"
-import ContactUs from "../components/contact/ContactUs"
-import VehicleBanner from "../components/vehicle/VehicleBanner"
+import HeroBreadcrumb from "../components/HeroBreadcrumb";
+import ContactUs from "../components/contact/ContactUs";
+import VehicleBanner from "../components/vehicle/VehicleBanner";
 
-export default function Contacts(){
-    return(
-        <section className="contacts py-5">
-        <HeroBreadcrumb currentPage={"Contacts"}/>
-        <ContactUs />
-        <VehicleBanner />
-        </section>
-    )
+export default function Contacts() {
+  return (
+    <section className="contacts pt-5">
+      <HeroBreadcrumb currentPage={"Contacts"} />
+      <ContactUs />
+      <VehicleBanner />
+      {/* map */}
+      <iframe
+        className="mt-5"
+        width="100%"
+        height="510"
+        allowFullScreen=""
+        loading="lazy"
+        id="gmap_canvas"
+        samesite="Strict"
+        src="https://maps.google.com/maps?q=kathmandu&t=&z=10&ie=UTF8&iwloc=&output=embed"
+        frameborder="0"
+      ></iframe>
+    </section>
+  );
 }
