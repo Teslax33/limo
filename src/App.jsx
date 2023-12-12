7e0010c1cb8a2f15875675ced8c15b75da54f184
Sadim Mali <sadimmalakar77@gmail.com>
2023-12-12 15:39:05 +0545

    feat : Add route to element components to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState, createContext } from "react";
-import Home from "./pages/Home";
+import { createContext, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import NavbarNav from "./components/NavbarNav";
-import Vehicles from "./pages/Vehicles";
 import Newsletter from "./components/service/Newsletter";
+import Blog from "./pages/Blog";
 import Contacts from "./pages/Contacts";
+import Home from "./pages/Home";
+import Vehicles from "./pages/Vehicles";
 
 export const CarContext = createContext("");
 
@@ -14,14 +15,14 @@ export default function App() {
 
   return (
     <CarContext.Provider value={{ show, setShow, isReserve, setIsReserve }}>
-    <NavbarNav />
-    <Routes>
-      <Route index path="/" element={<Home />} />
-      <Route path="/vehicles" element={<Vehicles/>} />
-      {/* <Route path="/blog" /> */}
-      <Route path="/contacts" element={<Contacts />} />
-    </Routes>
-    <Newsletter />
+      <NavbarNav />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/vehicles" element={<Vehicles />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+      <Newsletter />
     </CarContext.Provider>
   );
 }
